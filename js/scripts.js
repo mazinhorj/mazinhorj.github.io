faf75d4928e85f6a5776b2d2284d42355facfb2f
Osmar Menezes da Silva <adm.osmarsilva@gmail.com>
2022-12-05 22:09:23 -0300

    acoordeon

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,24 @@
 console.log("@MazinhoBigDaddy chegou!");
+
+//selects
+const cardBtn = document.querySelector(".card_acordeon i");
+const cardTabuada = document.querySelector("#tabuada");
+
+
+
+
+
+//funcs
+const abreFecha = (e) => {
+  cardBtn.classList.toggle("spin");
+  cardTabuada.classList.toggle("esconde");
+
+}
+
+
+
+//events
+
+cardBtn.addEventListener("click", (e) => {
+  abreFecha();
+})
