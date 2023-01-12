0b9acf6b982e65f43f2271a6c6d987f49a733f34
mazinhorj <65859903+mazinhorj@users.noreply.github.com>
2023-01-11 21:48:56 -0300

    novos projetos

--- js/scripts.js
+++ js/scripts.js
@@ -1,163 +1,335 @@
 console.log("@MazinhoBigDaddy chegou!");
 
 
-//selects
-const cardBtnSme = document.querySelector("#card_sme i");
+//selects + funcs + events
+
+// SME
+const cardBtnSme = document.querySelector("#card_sme i"); 
 const cardSme = document.querySelector("#cont_sme");
 
-const cardBtnTabuada = document.querySelector("#card_tabuada i");
-const cardTabuada = document.querySelector("#cont_tabuada");
-
-const cardBtnAdvogados = document.querySelector("#card_advogados i");
-const cardAdvogados = document.querySelector("#cont_advogados");
-
-const cardBtnQr = document.querySelector("#card_qr i");
-const cardQr = document.querySelector("#cont_qr");
-
-const cardBtnToDo = document.querySelector("#card_todo i");
-const cardToDo = document.querySelector("#cont_todo");
-
-const cardBtnSenha = document.querySelector("#card_senha i");
-const cardSenha = document.querySelector("#cont_senha");
-
-const cardBtnForm1 = document.querySelector("#card_form1 i");
-const cardForm1 = document.querySelector("#cont_form1");
-
-const cardBtnCalcImcV1 = document.querySelector("#card_calcimcv1 i");
-const cardCalcImcV1 = document.querySelector("#cont_calcimcv1");
-
-const cardBtnCalcImcV2 = document.querySelector("#card_calcimcv2 i");
-const cardCalcImcV2 = document.querySelector("#cont_calcimcv2");
-
-const cardBtnClima = document.querySelector("#card_clima i");
-const cardClima = document.querySelector("#cont_clima");
-
-const cardBtnCuriAndro = document.querySelector("#card_curiandro i");
-const cardCuriAndro = document.querySelector("#cont_curiandro");
-
-const cardBtnCordel = document.querySelector("#card_cordel i");
-const cardCordel = document.querySelector("#cont_cordel");
-
-const cardBtnQuiz = document.querySelector("#card_quiz i");
-const cardQuiz = document.querySelector("#cont_quiz");
-
-
-//funcs
 const abreFechaSme = (e) => {
   cardBtnSme.classList.toggle("spin");
   cardSme.classList.toggle("esconde");
 };
+
+cardBtnSme.addEventListener("click", (e) => {
+  abreFechaSme();
+});
+
+
+// ------------------------------------------------------------------------------
+
+// Tabuada
+const cardBtnTabuada = document.querySelector("#card_tabuada i");
+const cardTabuada = document.querySelector("#cont_tabuada");
 
 const abreFechaTabuada = (e) => {
   cardBtnTabuada.classList.toggle("spin");
   cardTabuada.classList.toggle("esconde");
 };
 
+cardBtnTabuada.addEventListener("click", (e) => {
+  abreFechaTabuada();
+});
+
+
+// ------------------------------------------------------------------------------
+
+// Site Advogados
+const cardBtnAdvogados = document.querySelector("#card_advogados i");
+const cardAdvogados = document.querySelector("#cont_advogados");
+
 const abreFechaAdvogados = (e) => {
   cardBtnAdvogados.classList.toggle("spin");
   cardAdvogados.classList.toggle("esconde");
 };
+
+cardBtnAdvogados.addEventListener("click", (e) => {
+  abreFechaAdvogados();
+});
+
+
+// ------------------------------------------------------------------------------
+
+// QR Code
+const cardBtnQr = document.querySelector("#card_qr i");
+const cardQr = document.querySelector("#cont_qr");
 
 const abreFechaQr = (e) => {
   cardBtnQr.classList.toggle("spin");
   cardQr.classList.toggle("esconde");
 };
 
+cardBtnQr.addEventListener("click", (e) => {
+  abreFechaQr();
+});
+
+
+// ------------------------------------------------------------------------------
+
+// ToDo
+const cardBtnToDo = document.querySelector("#card_todo i");
+const cardToDo = document.querySelector("#cont_todo");
+
 const abreFechaToDo = (e) => {
   cardBtnToDo.classList.toggle("spin");
   cardToDo.classList.toggle("esconde");
 };
+
+cardBtnToDo.addEventListener("click", (e) => {
+  abreFechaToDo();
+});
+
+
+// ------------------------------------------------------------------------------
+
+// Gerador de senha
+const cardBtnSenha = document.querySelector("#card_senha i");
+const cardSenha = document.querySelector("#cont_senha");
 
 const abreFechaSenha = (e) => {
   cardBtnSenha.classList.toggle("spin");
   cardSenha.classList.toggle("esconde");
 };
 
+cardBtnSenha.addEventListener("click", (e) => {
+  abreFechaSenha();
+});
+
+
+// ------------------------------------------------------------------------------
+
+// Form multi v1
+const cardBtnForm1 = document.querySelector("#card_form1 i");
+const cardForm1 = document.querySelector("#cont_form1");
+
 const abreFechaForm1 = (e) => {
   cardBtnForm1.classList.toggle("spin");
   cardForm1.classList.toggle("esconde");
 };
+
+cardBtnForm1.addEventListener("click", (e) => {
+  abreFechaForm1();
+});
+
+
+// ------------------------------------------------------------------------------
+
+// Form multi v2
+const cardBtnForm2 = document.querySelector("#card_form2 i");
+const cardForm2 = document.querySelector("#cont_form2");
+
+const abreFechaForm2 = (e) => {
+  cardBtnForm2.classList.toggle("spin");
+  cardForm2.classList.toggle("esconde");
+};
+
+cardBtnForm2.addEventListener("click", (e) => {
+  abreFechaForm2();
+});
+
+
+// ------------------------------------------------------------------------------
+
+// Calc IMC V1
+const cardBtnCalcImcV1 = document.querySelector("#card_calcimcv1 i");
+const cardCalcImcV1 = document.querySelector("#cont_calcimcv1");
 
 const abreFechaCalcImcV1 = (e) => {
   cardBtnCalcImcV1.classList.toggle("spin");
   cardCalcImcV1.classList.toggle("esconde");
 };
 
+cardBtnCalcImcV1.addEventListener("click", (e) => {
+  abreFechaCalcImcV1();
+});
+
+
+// ------------------------------------------------------------------------------
+
+// Calc IMC V2
+const cardBtnCalcImcV2 = document.querySelector("#card_calcimcv2 i");
+const cardCalcImcV2 = document.querySelector("#cont_calcimcv2");
+
 const abreFechaCalcImcV2 = (e) => {
   cardBtnCalcImcV2.classList.toggle("spin");
   cardCalcImcV2.classList.toggle("esconde");
 };
+
+cardBtnCalcImcV2.addEventListener("click", (e) => {
+  abreFechaCalcImcV2();
+});
+
+
+// ------------------------------------------------------------------------------
+
+// API clima
+const cardBtnClima = document.querySelector("#card_clima i");
+const cardClima = document.querySelector("#cont_clima");
 
 const abreFechaClima = (e) => {
   cardBtnClima.classList.toggle("spin");
   cardClima.classList.toggle("esconde");
 };
 
+cardBtnClima.addEventListener("click", (e) => {
+  abreFechaClima();
+});
+
+
+// ------------------------------------------------------------------------------
+
+// Curiosidades Android
+const cardBtnCuriAndro = document.querySelector("#card_curiandro i");
+const cardCuriAndro = document.querySelector("#cont_curiandro");
+
 const abreFechaCuriAndro = (e) => {
   cardBtnCuriAndro.classList.toggle("spin");
   cardCuriAndro.classList.toggle("esconde");
 };
+
+cardBtnCuriAndro.addEventListener("click", (e) => {
+  abreFechaCuriAndro();
+});
+
+
+// ------------------------------------------------------------------------------
+
+// Cordel
+const cardBtnCordel = document.querySelector("#card_cordel i");
+const cardCordel = document.querySelector("#cont_cordel");
 
 const abreFechaCordel = (e) => {
   cardBtnCordel.classList.toggle("spin");
   cardCordel.classList.toggle("esconde");
 };
 
+cardBtnCordel.addEventListener("click", (e) => {
+  abreFechaCordel();
+});
+
+
+// ------------------------------------------------------------------------------
+
+// Quiz
+const cardBtnQuiz = document.querySelector("#card_quiz i");
+const cardQuiz = document.querySelector("#cont_quiz");
+
 const abreFechaQuiz = (e) => {
   cardBtnQuiz.classList.toggle("spin");
   cardQuiz.classList.toggle("esconde");
 };
 
-
-//events
-cardBtnSme.addEventListener("click", (e) => {
-  abreFechaSme();
-});
-
-cardBtnTabuada.addEventListener("click", (e) => {
-  abreFechaTabuada();
-});
-
-cardBtnAdvogados.addEventListener("click", (e) => {
-  abreFechaAdvogados();
-});
-
-cardBtnQr.addEventListener("click", (e) => {
-  abreFechaQr();
-});
-
-cardBtnToDo.addEventListener("click", (e) => {
-  abreFechaToDo();
-});
-
-cardBtnSenha.addEventListener("click", (e) => {
-  abreFechaSenha();
-});
-
-cardBtnForm1.addEventListener("click", (e) => {
-  abreFechaForm1();
-});
-
-cardBtnCalcImcV1.addEventListener("click", (e) => {
-  abreFechaCalcImcV1();
-});
-
-cardBtnCalcImcV2.addEventListener("click", (e) => {
-  abreFechaCalcImcV2();
-});
-
-cardBtnClima.addEventListener("click", (e) => {
-  abreFechaClima();
-});
-
-cardBtnCuriAndro.addEventListener("click", (e) => {
-  abreFechaCuriAndro();
-});
-
-cardBtnCordel.addEventListener("click", (e) => {
-  abreFechaCordel();
-});
-
 cardBtnQuiz.addEventListener("click", (e) => {
   abreFechaQuiz();
 });
+
+
+// ------------------------------------------------------------------------------
+
+// Git buscador
+const cardBtnGit = document.querySelector("#card_git i");
+const cardGit = document.querySelector("#cont_git");
+
+const abreFechaGit = (e) => {
+  cardBtnGit.classList.toggle("spin");
+  cardGit.classList.toggle("esconde");
+};
+
+cardBtnGit.addEventListener("click", (e) => {
+  abreFechaGit();
+});
+
+
+// ------------------------------------------------------------------------------
+
+// Promessas
+const cardBtnCaixinha = document.querySelector("#card_caixinha i");
+const cardCaixinha = document.querySelector("#cont_caixinha");
+
+const abreFechaCaixinha = (e) => {
+  cardBtnCaixinha.classList.toggle("spin");
+  cardCaixinha.classList.toggle("esconde");
+};
+
+cardBtnCaixinha.addEventListener("click", (e) => {
+  abreFechaCaixinha();
+});
+
+
+// ------------------------------------------------------------------------------
+
+// Postits
+const cardBtnPostits = document.querySelector("#card_postits i");
+const cardPostits = document.querySelector("#cont_postits");
+
+const abreFechaPostits = (e) => {
+  cardBtnPostits.classList.toggle("spin");
+  cardPostits.classList.toggle("esconde");
+};
+
+cardBtnPostits.addEventListener("click", (e) => {
+  abreFechaPostits();
+});
+
+
+// ------------------------------------------------------------------------------
+
+// Lazy
+const cardBtnLazy = document.querySelector("#card_lazy i");
+const cardLazy = document.querySelector("#cont_lazy");
+
+const abreFechaLazy = (e) => {
+  cardBtnLazy.classList.toggle("spin");
+  cardLazy.classList.toggle("esconde");
+};
+
+cardBtnLazy.addEventListener("click", (e) => {
+  abreFechaLazy();
+});
+
+
+// ------------------------------------------------------------------------------
+
+// iphone
+const cardBtnIphone = document.querySelector("#card_iphone i");
+const cardIphone = document.querySelector("#cont_iphone");
+
+const abreFechaIphone = (e) => {
+  cardBtnIphone.classList.toggle("spin");
+  cardIphone.classList.toggle("esconde");
+};
+
+cardBtnIphone.addEventListener("click", (e) => {
+  abreFechaIphone();
+});
+
+
+// ------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
